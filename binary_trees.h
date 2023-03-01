db9c794b26a1bac752c352b52b78f6732716bd03
lawal toheeb damilare <lawaltd088@gmail.com>
2023-03-01 16:01:55 +0000

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,10 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,3 +22,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+#endif
